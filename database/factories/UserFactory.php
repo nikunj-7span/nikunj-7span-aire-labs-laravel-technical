de77ef3c711174b22da0aa7ab5781c9197da3567
nikunj-7span <nikunj@7span.com>
2023-08-29 11:33:15 +0530

    Update user factory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,12 +17,18 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $imageDirectory = storage_path('app/public/images');
+        $images = glob($imageDirectory . '/*.jpg');
+        $randomImagePath = $images[array_rand($images)];
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'type' => fake()->randomElement(['Founder', 'CEO', 'Consultant']),
+            'avatar' =>  'images/' . basename($randomImagePath)
         ];
     }
 
